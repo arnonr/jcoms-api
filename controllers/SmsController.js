560cfba89534ae720bfccdb965699b298401673d
arnonr <arnonr@kmutnb.ac.th>
2024-03-27 23:01:34 +0700

    update SMS

--- controllers/SmsController.js
+++ controllers/SmsController.js
@@ -1,49 +1,49 @@
-
 const axios = require("axios");
 
 const methods = {
-    async onSendSms(req, res) {
-        // console.log(req.body)
-        let msisdn = req.body.msisdn;
-        let message = req.body.message;
+  async onSendSms(req, res) {
+    // console.log(req.body)
+    let msisdn = req.body.msisdn;
+    let message = req.body.message;
 
-        if (msisdn == undefined) {
-            return res.status(400).json({ msg: "msisdn is undefined" });
+    if (msisdn == undefined) {
+      return res.status(400).json({ msg: "msisdn is undefined" });
+    }
+
+    if (message == undefined) {
+      return res.status(400).json({ msg: "message is undefined" });
+    }
+
+    try {
+      let params = {
+        msisdn: msisdn,
+        message: message,
+        sender: "Demo",
+        force: "corporate",
+        // Shorten_url: true,
+        // tracking_url: true,
+        // expire:
+      };
+      let auth = {
+        username: process.env.SMS_USERNAME,
+        password: process.env.SMS_PASSWORD,
+      };
+
+      const sms = await axios.post(
+        `https://api-v2.thaibulksms.com/sms`,
+        params,
+        {
+          auth: auth,
         }
+      );
 
-        if (message == undefined) {
-            return res.status(400).json({ msg: "message is undefined" });
-        }
+      // console.log(sms.data);
 
-        try {
-
-            let params = {
-                msisdn: msisdn,
-                message: message,
-                sender: "Demo",
-                force: "standard",
-                // Shorten_url: true,
-                // tracking_url: true,
-                // expire:
-            };
-
-            let auth = {
-                username: "tRYlZ4Ddn8dOKUwCRBgASLMg5vDMLQ",
-                password: "8GgqEuR50OeQkCiSIrfgJ58X84IAbt",
-            };
-
-            const sms = await axios.post(`https://api-v2.thaibulksms.com/sms`, params, {
-                auth: auth,
-            });
-
-            // console.log(sms.data);
-
-            res.status(200).json({ ...params, msg: "success" });
-
-        } catch (error) {
-            res.status(400).json({ msg: error.message });
-        }
-    },
+      res.status(200).json({ ...params, msg: "success" });
+    } catch (error) {
+      res.status(400).json({ msg: error.message });
+    }
+  },
 };
 
 module.exports = { ...methods };
