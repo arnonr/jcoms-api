ee4d247d0064c9aa5ceecf1751e0fc0b353f4390
Siwakorn longsomboon <siwakorn.l@icit.kmutnb.ac.th>
2024-03-28 09:54:38 +0700

    SMS

--- controllers/SmsController.js
+++ controllers/SmsController.js
@@ -1,49 +1,48 @@
 const axios = require("axios");
 
 const methods = {
-  async onSendSms(req, res) {
-    // console.log(req.body)
-    let msisdn = req.body.msisdn;
-    let message = req.body.message;
+    async onSendSms(req, res) {
+        // console.log(req.body)
+        let msisdn = req.body.msisdn;
+        let message = req.body.message;
 
-    if (msisdn == undefined) {
-      return res.status(400).json({ msg: "msisdn is undefined" });
-    }
-
-    if (message == undefined) {
-      return res.status(400).json({ msg: "message is undefined" });
-    }
-
-    try {
-      let params = {
-        msisdn: msisdn,
-        message: message,
-        sender: "Demo",
-        force: "corporate",
-        // Shorten_url: true,
-        // tracking_url: true,
-        // expire:
-      };
-      let auth = {
-        username: process.env.SMS_USERNAME,
-        password: process.env.SMS_PASSWORD,
-      };
-
-      const sms = await axios.post(
-        `https://api-v2.thaibulksms.com/sms`,
-        params,
-        {
-          auth: auth,
+        if (msisdn == undefined) {
+        return res.status(400).json({ msg: "msisdn is undefined" });
         }
-      );
 
-      // console.log(sms.data);
+        if (message == undefined) {
+        return res.status(400).json({ msg: "message is undefined" });
+        }
 
-      res.status(200).json({ ...params, msg: "success" });
-    } catch (error) {
-      res.status(400).json({ msg: error.message });
-    }
-  },
+        try {
+            let params = {
+                msisdn: msisdn,
+                message: message,
+                sender: "Demo",
+                force: "corporate",
+                // Shorten_url: true,
+                // tracking_url: true,
+                // expire:
+            };
+            let auth = {
+                username: process.env.SMS_USERNAME,
+                password: process.env.SMS_PASSWORD,
+            };
+
+            const sms = await axios.post(
+                `https://api-v2.thaibulksms.com/sms`,
+                params,{
+                    auth: auth,
+                }
+            );
+
+            // console.log(sms.data);
+
+            res.status(200).json({ ...params, msg: "success" });
+        } catch (error) {
+            res.status(400).json({ msg: error.message });
+        }
+    },
 };
 
 module.exports = { ...methods };
