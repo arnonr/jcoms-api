13f903db45aa5ef6673d18ce9a1dae8c6b635886
Siwakorn longsomboon <siwakorn.l@icit.kmutnb.ac.th>
2024-03-28 23:34:16 +0700

    post tracking

--- routes/api/complaint.js
+++ routes/api/complaint.js
@@ -5,7 +5,7 @@ const controllers = require("../../controllers/ComplaintController");
 
 router.get("/", controllers.onGetAll);
 
-router.get("/get-otp-tracking", controllers.onGetOTPTracking);
+router.post("/get-otp-tracking", controllers.onGetOTPTracking);
 router.post("/verify-otp-tracking", controllers.onVertifyOTPTracking);
 router.get("/get-complaint-by-otp", controllers.onGetAllByOTP);
 router.get("/:id", controllers.onGetById);
