74ec3f11df83cdacef14de75940219c8d80d7fe2
arnonr <arnonr@kmutnb.ac.th>
2024-07-20 19:40:14 +0700

    update

--- controllers/ComplainantController.js
+++ controllers/ComplainantController.js
@@ -297,11 +297,12 @@ const methods = {
           
             const item = await prisma[$table].findMany({
                 // select: selectField,
-                // where: $where,
+                where: $where,
                 // orderBy: other.$orderBy,
                 // skip: other.$offset,
                 // take: other.$perPage,
             });
+            
 
 
             res.status(200).json({
