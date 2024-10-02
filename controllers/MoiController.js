5972bb1cf12255d575d0ddb46245391fb55d88cd
arnonr <arnonr@kmutnb.ac.th>
2024-10-02 10:45:32 +0700

    update

--- controllers/MoiController.js
+++ controllers/MoiController.js
@@ -16,8 +16,8 @@ const helperController = require("./HelperController");
 const uploadController = require("./UploadsController");
 const { Console } = require('console');
 
-const apiHost = "http://damrongdham-api-uat.moi.go.th";
-const apiSecretKey = "iSOfG6mYMr";
+const apiHost = "damrongdham-api.moi.go.th";
+const apiSecretKey = "QlpQdZR1tK";
 const agentId = "A01";
 
 const getToken = async () => {
--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 const port = process.env.APP_PORT || 3002;
 
 const corsOptions = {
-  origin: "https://jcoms2.police.go.th",
+  origin: "*",
   credentials: true,
 };
 // app.use(helmet());
